0b118c05f8744adc9b6938fc5275f0d510f3bdd6
ghpopovici <ghpopovici@gmail.com>
2016-05-24 12:05:41 +0200

    recommit after some truble with Zookeeper server

---
+++ tech.ghp.chat.api/src/tech/ghp/chat/api/Chat.java
@@ -0,0 +1,19 @@
+package tech.ghp.chat.api;
+
+/**
+ * 
+ */
+
+import org.osgi.annotation.versioning.ProviderType;
+
+@ProviderType
+public interface Chat {
+	
+	/**
+	 * 
+	 */
+	String USER_NAME = "user.name";
+
+	boolean send(Message message) throws Exception;
+	
+}
